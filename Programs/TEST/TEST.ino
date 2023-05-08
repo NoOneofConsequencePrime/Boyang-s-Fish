f5b5de7c573f20a3f70e70ed71fd462afc5ffbb8
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2023-05-08 15:39:55 -0400

    Started Coding
    - Updated 3D designs
    - Ordered 50 RPM motors
    - Tested basic code (MPU works, should test for disconnect issues later on)

--- Programs/TEST/TEST.ino
+++ Programs/TEST/TEST.ino
@@ -1,5 +1,12 @@
 void setup() {
+    pinMode(26, OUTPUT);
+    Serial.begin(9600);
 }
 
 void loop() {
+  digitalWrite(26, HIGH);
+  delay(100);
+  digitalWrite(26, LOW);
+  delay(100);
+  Serial.println("Running");
 }
